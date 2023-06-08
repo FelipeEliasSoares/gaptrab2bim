e93f73cc2b8230eb020cac7b5632aed5bede430b
FelipeElias <soaresfelipe396@gmail.com>
2023-06-08 15:18:45 -0300

    Inicio

---
+++ ClinicaAdocao/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: felipe
+ *
+ * Created on 8 de junho de 2023, 15:18
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
