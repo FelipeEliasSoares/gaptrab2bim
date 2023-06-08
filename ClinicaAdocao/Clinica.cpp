7c6614503234ab7c4acd7794e9ab3672449abe56
FelipeElias <soaresfelipe396@gmail.com>
2023-06-08 15:19:21 -0300

    inicio

---
+++ ClinicaAdocao/Clinica.cpp
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.cc to edit this template
+ */
+
+/* 
+ * File:   Clinica.cpp
+ * Author: felipe
+ * 
+ * Created on 8 de junho de 2023, 15:19
+ */
+
+#include "Clinica.h"
+
+Clinica::Clinica() {
+}
+
+Clinica::Clinica(const Clinica& orig) {
+}
+
+Clinica::~Clinica() {
+}
+
---
+++ ClinicaAdocao/Clinica.h
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.h to edit this template
+ */
+
+/* 
+ * File:   Clinica.h
+ * Author: felipe
+ *
+ * Created on 8 de junho de 2023, 15:19
+ */
+
+#ifndef CLINICA_H
+#define CLINICA_H
+
+class Clinica {
+public:
+    Clinica();
+    Clinica(const Clinica& orig);
+    virtual ~Clinica();
+private:
+
+};
+
+#endif /* CLINICA_H */
+
